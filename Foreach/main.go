dcb3a0b8aa634cceb1ac7eb4b051c7b1c84cf21f
Ahmed Asfour <63642852+ahmetasfour@users.noreply.github.com>
2024-09-13 14:25:14 +0300

    for

--- Foreach/main.go
+++ Foreach/main.go
@@ -4,7 +4,7 @@ import "fmt"
 
 func main() {
 
-	//var numbers = []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 9, 0, 9, 0, 98, 9898}
+	var numbers = []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 9, 0, 9, 0, 98, 9898}
 	/*
 		for i := 0; i < len(numbers); i++ {
 
@@ -12,16 +12,14 @@ func main() {
 		}
 	*/
 
-	/*
-		for index, value := range numbers {
+	for index, value := range numbers {
 
-			fmt.Println(index, value)
-		}
-	*/
+		fmt.Println(index, value)
+	}
 
 	var language = "golang"
 	for _, count := range language {
-		fmt.Printf("%c\n", count)
+		fmt.Printf("%c", count)
 
 	}
 
